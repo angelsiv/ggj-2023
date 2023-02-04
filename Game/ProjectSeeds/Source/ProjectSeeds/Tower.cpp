4d5aa9c7ed9482d88c9c89960b8c686afa9cf6a6
Angeline <siv.angeline@ymail.com>
2023-02-04 03:23:05 -0500

    Add Tower (WIP)

---
+++ Game/ProjectSeeds/Source/ProjectSeeds/Tower.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tower.h"
+
+#include "Components/MeshComponent.h"
+#include "Components/SphereComponent.h"
+
+ATower::ATower()
+{
+	bCanMove = false;
+	bCanFire = true;
+	SeedFaction = ESeedFaction::FactionEnemy;
+
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCollider"));
+	SphereComponent->SetupAttachment(RootComponent);
+	SphereComponent->InitSphereRadius(RangeRadius);
+}
+
+void ATower::ChangeFaction()
+{
+	(SeedFaction == ESeedFaction::FactionEnemy)
+		? SeedFaction = ESeedFaction::FactionPlayer
+		: SeedFaction = ESeedFaction::FactionEnemy;
+}
+
+void ATower::FireShot()
+{
+	if (Target != nullptr)
+	{
+		// Shoot at target
+		// Need some bullet pool...
+	}
+}
+
+void ATower::OnComponentOverlap(UPrimitiveComponent* OverlappedComp, AActor* Other, UPrimitiveComponent* OtherComp,
+                                int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+
+{
+	if(auto* Entity = Cast<ABaseSeedAI>(Other))
+	{
+		if (Entity->SeedFaction != SeedFaction) // Verify target is of different faction
+		{
+			Target = Entity;
+			FireShot();
+		}
+	};
+}
+
+void ATower::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	SphereComponent->OnComponentBeginOverlap.AddDynamic(this, &ATower::OnComponentOverlap);
+}
